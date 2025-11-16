40c47c7f106596fc0f27c99fcc7d3994b3c4fe33
NjengaSheba <njengasheba@gmail.com>
2025-11-16 18:13:08 +0300

    Added 'receive_kobo_submission' function, mapped kobo headers with frappe fields

--- onerc_relief/api/kobo.py
+++ onerc_relief/api/kobo.py
@@ -1,5 +1,4 @@
-import requests
-import frappe
+import frappe, json, base64, requests
 
 from frappe.utils.password import get_decrypted_password
 
@@ -8,7 +7,59 @@ kobo_settings = frappe.get_single("Kobo Settings")
 TOKEN = get_decrypted_password("Kobo Settings", name = "Kobo Settings", fieldname = "token")
 ASSET_UID = kobo_settings.asset_uid
 URL = f'{kobo_settings.url}/api/v2/assets/{ASSET_UID}/data'
+
 headers = {"Authorization": f"Token {TOKEN}"}
 
-def fetch_kobo_data():
-    pass
+
+@frappe.whitelist(allow_guest=True)
+def receive_kobo_submission():
+    data = frappe.request.get_json()
+    if not data:
+        frappe.throw("No Data Received")
+
+
+    headers = dict(frappe.request.headers)
+
+    doctype = headers.get("doctype")
+    if not doctype:
+        frappe.throw("'doctype' header not found!")
+    
+    # if data.get("skipconnect") == "1":
+    #     return {"status": "skipped"}
+
+    # Mapp the headers from kobo onto frappe
+
+    doc_fields = {}
+    for kobo_field, frappe_field in headers.items():
+
+        # Ignore system headers
+        if kobo_field.lower() in [
+            "content-type", "user-agent", "host", "doctype", "updateby", "authorization"
+        ]:
+            continue
+
+
+
+        if kobo_field in data:
+            doc_fields[frappe_field] = data[kobo_field]
+
+    existing = None
+    if "updateby" in headers:
+        fieldname = headers["updateby"]
+        existing = frappe.db.exists(doctype, {fieldname: doc_fields.get(fieldname)})
+    
+    if existing:
+        doc = frappe.get_doc(doctype, existing)
+        doc.update(doc_fields)
+    else:
+        doc = frappe.get_doc({
+            "doctype": doctype,
+            **doc_fields
+        })
+
+
+    doc.save(ignore_permissions = True)    
+    frappe.db.commit()
+
+    return {"status": "success", "name": doc.name}
+
