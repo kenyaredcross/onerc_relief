15a40c1e50c150e93787ef085395e3e01558a4fc
NjengaSheba <njengasheba@gmail.com>
2025-11-28 21:54:35 +0300

    added new 121 project doctype.

---
+++ onerc_relief/onerc_relief/doctype/new_121_project/new_121_project.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, NjengaSheba and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class New121Project(Document):
+	pass
